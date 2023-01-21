84b89c65b3b76448a3353e4c9211b1b4799d6ee2
WebbLua <121877043+WebbLua@users.noreply.github.com>
2023-01-21 15:28:37 +0100

    Add files via upload

--- SRPmembers.lua
+++ SRPmembers.lua
@@ -1,9 +1,9 @@
 script_name('SRPmembers')
 script_author("Cody_Webb | Telegram: @Imikhailovich")
 script_version("21.01.2023")
-script_version_number(4)
+script_version_number(5)
 local script = {checked = false, available = false, update = false, v = {date, num}, url, reload, loaded, unload, quest = {}, upd = {changes = {}, sort = {}}, label = {}}
-local check = {bool = false, boolstream = false, stream = {}, findstream = false, status = false, amount = 0, irank = {}, line = 0, rmembers = {}, current = {}}
+local check = {bool = false, boolstream = false, stream = {}, findstream = false, status = false, amount = 0, irank = {}, line = 0, rmembers = {}, current = {}, mem1 = {}}
 -------------------------------------------------------------------------[Библиотеки/Зависимости]---------------------------------------------------------------------
 local ev = require 'samp.events'
 local imgui = require 'imgui'
@@ -39,7 +39,8 @@ local menu = { -- imgui-меню
 	main = imgui.ImBool(false),
 	settings = imgui.ImBool(true),
 	information = imgui.ImBool(false),
-	commands = imgui.ImBool(false)
+	commands = imgui.ImBool(false),
+	members = imgui.ImBool(false)
 }
 imgui.ShowCursor = false
 
@@ -49,7 +50,8 @@ local clr = imgui.Col
 local currentNick
 local suspendkeys = 2 -- 0 хоткеи включены, 1 -- хоткеи выключены -- 2 хоткеи необходимо включить
 local ImVec4 = imgui.ImVec4
-local imfonts = {mainFont = nil, smallmainFont = nil}
+local imfonts = {mainFont = nil, smallmainFont = nil, memfont = nil}
+local mem1 = {[1] = {}, [2] = {}, [3] ={}, [4] = {}, [5] = {}}
 -------------------------------------------------------------------------[MAIN]--------------------------------------------------------------------------------------------
 function main()
 	if not isSampLoaded() or not isSampfuncsLoaded() then return end
@@ -95,6 +97,7 @@ function main()
 	end)
 	sampRegisterChatCommand('getstream', getstream)
 	sampRegisterChatCommand('membup', updateScript)
+	sampRegisterChatCommand('mem1', cmd_mem1)
 	
 	script.loaded = true
 	repeat wait(0) until sampIsLocalPlayerSpawned()
@@ -205,11 +208,71 @@ function apply_custom_styles()
 	imgui.GetIO().Fonts:Clear()
 	imfonts.mainFont = imgui.GetIO().Fonts:AddFontFromFileTTF(getFolderPath(0x14)..'\\times.ttf', 20.0, nil, imgui.GetIO().Fonts:GetGlyphRangesCyrillic())
 	imfonts.smallmainFont = imgui.GetIO().Fonts:AddFontFromFileTTF(getFolderPath(0x14)..'\\times.ttf', 16.0, nil, imgui.GetIO().Fonts:GetGlyphRangesCyrillic())
+	imfonts.memfont = imgui.GetIO().Fonts:AddFontFromFileTTF(getFolderPath(0x14)..'\\times.ttf', 20.0, nil, imgui.GetIO().Fonts:GetGlyphRangesCyrillic())
 	
 	imgui.GetIO().Fonts:AddFontFromFileTTF(getFolderPath(0x14)..'\\times.ttf', 14.0, nil, imgui.GetIO().Fonts:GetGlyphRangesCyrillic())
 	imgui.RebuildFonts()
 end
 apply_custom_styles()
+
+function imgui.TextColoredRGB(text)
+	local style = imgui.GetStyle()
+	local colors = style.Colors
+	local ImVec4 = imgui.ImVec4
+	
+	local explode_argb = function(argb)
+		local a = bit.band(bit.rshift(argb, 24), 0xFF)
+		local r = bit.band(bit.rshift(argb, 16), 0xFF)
+		local g = bit.band(bit.rshift(argb, 8), 0xFF)
+		local b = bit.band(argb, 0xFF)
+		return a, r, g, b
+	end
+	
+	local getcolor = function(color)
+		if color:sub(1, 6):upper() == 'SSSSSS' then
+			local r, g, b = colors[1].x, colors[1].y, colors[1].z
+			local a = tonumber(color:sub(7, 8), 16) or colors[1].w * 255
+			return ImVec4(r, g, b, a / 255)
+		end
+		
+		local color = type(color) == 'string' and tonumber(color, 16) or color
+		if type(color) ~= 'number' then return end
+		local r, g, b, a = explode_argb(color)
+		return imgui.ImColor(r, g, b, a):GetVec4()
+	end
+	
+	local render_text = function(text_)
+		for w in text_:gmatch('[^\r\n]+') do
+			local text, colors_, m = {}, {}, 1
+			w = w:gsub('{(......)}', '{%1FF}')
+			while w:find('{........}') do
+				local n, k = w:find('{........}')
+				local color = getcolor(w:sub(n + 1, k - 1))
+				if color then
+					text[#text], text[#text + 1] = w:sub(m, n - 1), w:sub(k + 1, #w)
+					colors_[#colors_ + 1] = color
+					m = n
+				end
+				
+				w = w:sub(1, n - 1) .. w:sub(k + 1, #w)
+			end
+			
+			if text[0] then
+				for i = 0, #text do
+					imgui.TextColored(colors_[i] or colors[1], text[i])
+					if imgui.IsItemClicked() then	if SelectedRow == A_Index then ChoosenRow = SelectedRow	else	SelectedRow = A_Index	end	end
+					imgui.SameLine(nil, 0)
+				end
+				
+				imgui.NewLine()
+				else
+				imgui.Text(w)
+				if imgui.IsItemClicked() then	if SelectedRow == A_Index then ChoosenRow = SelectedRow	else	SelectedRow = A_Index	end	end
+			end
+		end
+	end
+	render_text(text)
+end
 
 function imgui.OnDrawFrame()
 	if menu.main.v and script.checked then -- меню скрипта
@@ -266,6 +329,7 @@ function imgui.OnDrawFrame()
 				"/memb - открыть/закрыть главное меню скрипта",
 				"/getstream - проверить наличие игроков организации в зоне прорисовке",
 				"/membup - обновить скрипт",
+				"/mem1 - посмотреть мемберс в красивом imgui-окне"
 			}
 			local w = 0
 			local sortcmds = {}
@@ -312,6 +376,37 @@ function imgui.OnDrawFrame()
 		imgui.End()
 		imgui.PopFont()
 	end
+	if menu.members.v then
+		imgui.SwitchContext()
+		colors[clr.WindowBg] = ImVec4(0.06, 0.06, 0.06, 0.94)
+		imgui.PushFont(imfonts.memfont)
+		local sw, sh = getScreenResolution()
+		imgui.SetNextWindowPos(imgui.ImVec2(sw / 2, sh / 2), imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))
+		imgui.SetNextWindowSize(imgui.ImVec2(650, 540), imgui.Cond.FirstUseEver, imgui.WindowFlags.NoCollapse + imgui.WindowFlags.NoResize + imgui.WindowFlags.NoScrollbar)
+		imgui.Begin("Состав онлайн", menu.members, imgui.WindowFlags.NoCollapse + imgui.WindowFlags.NoResize + imgui.WindowFlags.NoScrollbar)
+		imgui.Columns(5, 1, true)
+		imgui.TextColoredRGB("{FFFAFA}#")
+		for k, v in ipairs(mem1[1]) do imgui.TextColoredRGB("{FFFAFA}" .. v .. "") end
+		imgui.NextColumn()
+		imgui.TextColoredRGB("{FFFAFA}ID")
+		for k, v in ipairs(mem1[2]) do imgui.TextColoredRGB("{FFFAFA}" .. v .. "") end
+		imgui.NextColumn()
+		imgui.TextColoredRGB("{FFFAFA}Ник")
+		for k, v in ipairs(mem1[3]) do imgui.TextColoredRGB("{FFFAFA}" .. v .. "") if imgui.IsItemClicked() then sampSetChatInputEnabled(true) sampSetChatInputText("/t " .. mem1[2][k] .. " ") end end
+		imgui.NextColumn()
+		imgui.TextColoredRGB("{FFFAFA}Ранг")
+		for k, v in ipairs(mem1[4]) do imgui.TextColoredRGB("{FFFAFA}" .. u8(v) .. "") end
+		imgui.NextColumn()
+		imgui.TextColoredRGB("{FFFAFA}АФК")
+		for k, v in ipairs(mem1[5]) do imgui.TextColoredRGB("{FFFAFA}" .. v .. "") end
+		imgui.SetColumnWidth(0, 40)
+		imgui.SetColumnWidth(1, 40)
+		imgui.SetColumnWidth(2, 200)
+		imgui.SetColumnWidth(3, 150)
+		imgui.ShowCursor = true
+		imgui.End()
+		imgui.PopFont()
+	end
 end
 -------------------------------------------------------------------------[ФУНКЦИИ]-----------------------------------------------------------------------------------------
 function ev.onServerMessage(col, text)
@@ -329,7 +424,7 @@ function ev.onServerMessage(col, text)
 					return false
 				end 
 			end
-			local nick, rank, i = text:match("^%[%d+%] (.*)  %{C0C0C0%}(.*) %[(.*)%]  %{6495ED%}")
+			local id, nick, rank, i, str = text:match("^%[(%d+)%] (.*)  %{C0C0C0%}(.*) %[(.*)%]  %{6495ED%}(.*)")
 			if i ~= nil then
 				if i:match(u8:decode"лидер") then 
 					i = 10
@@ -341,8 +436,10 @@ function ev.onServerMessage(col, text)
 					end
 				end
 			end
-			if nick ~= nil and rank ~= nil and tonumber(i) then 
+			if id ~= nil and nick ~= nil and rank ~= nil and tonumber(i) then 
 				srpmembers_ini.list[nick] = rank
+				local afk = str:match("(%[.*%])")
+				check.mem1[nick] = {id = id, nick = nick, rank = rank, irank = tonumber(i), afk = afk ~= nil and afk or nil}
 				check.current[nick] = rank
 				check.irank[rank] = tonumber(i)
 				if check.boolstream then 
@@ -357,7 +454,7 @@ function ev.onServerMessage(col, text)
 		if col == -1061109505 then
 			if text:match("^===========================================$") then 
 				if check.bool then 
-					if check.status then
+					if check.status or check.mem1status then
 						check.bool = false
 						removeFired()
 						if not check.boolstream then
@@ -501,6 +598,32 @@ function sampGetPlayerIdByNickname(name)
 			return i
 		end
 	end
+end
+
+function cmd_mem1()
+	lua_thread.create(function()
+		mem1 = {[1] = {}, [2] = {}, [3] ={}, [4] = {}, [5] = {}}
+		check.mem1 = {}
+		members()
+		while check.bool do wait(0) end
+		local n = 0
+		for k, v in pairs(check.mem1) do
+			if k ~= nil then
+				local afk = v.afk == nil and "" or v.afk
+				local clist = string.sub(string.format('%x', sampGetPlayerColor(v.id)), 3)
+				local clist = clist == "ffff" and "fffafa" or clist
+				local rankcol = v.irank >= 12 and "00BFFF" or (v.irank == 1 and clist == "fffafa") and "ff0000" or "fffafa"
+				n = n + 1
+				table.insert(mem1[1], n)
+				table.insert(mem1[2], v.id)
+				table.insert(mem1[3], "{" .. clist .. "}" .. k .. "")
+				table.insert(mem1[4], "{" .. rankcol .. "}" .. v.rank .. "[" .. v.irank .. "]")
+				table.insert(mem1[5], afk)
+			end
+		end
+		
+		menu.members.v = true
+	end)
 end
 -------------------------------------------[ChatManager -> взято из donatik.lua]------------------------------------------
 chatManager = {}
